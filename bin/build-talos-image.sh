224e52488a67b09217b60f5dcf2ce91156cdc32a
f-bn <contact@fbobin.me>
2024-10-21 15:58:01 +0200

    talos: add output dir for generated image

--- bin/build-talos-image.sh
+++ bin/build-talos-image.sh
@@ -12,8 +12,9 @@ BUILD_DIR="/tmp/incus-talos-build"
 BUILD_EPOCH="$(date +%s)"
 VERSION="${1:-v1.8.1}"
 SCHEMATIC_ID="376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba"
+OUTPUT_DIR="$PWD/output"
 
-mkdir -p ${BUILD_DIR}
+mkdir -p ${BUILD_DIR} ${OUTPUT_DIR}
 
 echo "* Downloading Talos Linux image (${VERSION})..."
 wget -q -O ${BUILD_DIR}/talos-${VERSION}.raw.xz \
@@ -34,7 +35,7 @@ properties:
   os: Talos Linux
   release: ${VERSION//v}
 EOF
-tar --zstd -cf talos-${VERSION}.tar.zst -C ${BUILD_DIR} metadata.yaml rootfs.img
+tar --zstd -cf ${OUTPUT_DIR}/talos-${VERSION}.tar.zst -C ${BUILD_DIR} metadata.yaml rootfs.img
 
 echo "* Cleaning up..."
 rm -rf ${BUILD_DIR}
