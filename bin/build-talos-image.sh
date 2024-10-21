6da2964565257c2ff2471233221f0938446c6abe
f-bn <contact@fbobin.me>
2024-10-21 15:56:51 +0200

    talos: update download URL to Image Factory

--- bin/build-talos-image.sh
+++ bin/build-talos-image.sh
@@ -10,13 +10,14 @@ set -eo pipefail
 
 BUILD_DIR="/tmp/incus-talos-build"
 BUILD_EPOCH="$(date +%s)"
-VERSION="${1:-v1.6.5}"
+VERSION="${1:-v1.8.1}"
+SCHEMATIC_ID="376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba"
 
 mkdir -p ${BUILD_DIR}
 
 echo "* Downloading Talos Linux image (${VERSION})..."
 wget -q -O ${BUILD_DIR}/talos-${VERSION}.raw.xz \
-  https://github.com/siderolabs/talos/releases/download/${VERSION}/nocloud-amd64.raw.xz
+  https://factory.talos.dev/image/${SCHEMATIC_ID}/${VERSION}/nocloud-amd64.raw.xz
 
 echo "* Extracting image..."
 xz -d ${BUILD_DIR}/talos-${VERSION}.raw.xz
